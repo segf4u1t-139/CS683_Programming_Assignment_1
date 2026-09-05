882a680e860ae00e22758e2aa000035520b0bfdb
segf4u1t-139 <methre.d1n35h@gmail.com>
2026-09-05 11:54:51 +0530

    task1: Implementing the Tiling, But not seeing any change in speedup (it might be becoming worse?)

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -4,6 +4,27 @@
 
 void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
-    // TODO(student): replace this placeholder with your tiled/blocked implementation.
-    conv_naive(in, out, ker, H, W, K);
+
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+    int tile_size = 48;
+
+    for (int Ty = 0; Ty < H; Ty += tile_size) {
+        int tile_end_y = Ty + tile_size < H ? Ty + tile_size : H;
+        for (int Tx = 0; Tx < W; Tx += tile_size) {
+            int tile_end_x = Tx + tile_size < W ? Tx + tile_size : W;
+
+            for (int oy = Ty; oy < tile_end_y; ++oy) {
+                for (int ox = Tx; ox < tile_end_x; ++ox) {
+                    float acc = 0.0f;
+                    for (int ky = 0; ky < K; ++ky) {
+                        for (int kx = 0; kx < K; ++kx) {
+                            acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+                        }
+                    }
+                    out[oy * W + ox] = acc;
+                }
+            }
+        }
+    }
 }
