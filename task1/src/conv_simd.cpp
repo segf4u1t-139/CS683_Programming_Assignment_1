025a3b50185083564e8153e73eaa20a1bed809b9
segf4u1t-139 <methre.d1n35h@gmail.com>
2026-09-06 04:54:32 +0530

    task1: Implemented the 128 Bit Width SIMD Instruction (and also caught a bug in the process?)

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -11,18 +11,31 @@ void conv_simd(const float* in, float* out, const float* ker,
 
     for (int oy = 0; oy < H; ++oy) {
 
-        // THE NEEDED IMPLEMENTATION FOR SIMD FOR 256 BITS (32 BYTES => 8 FLOAT VALUES WHICH WE KNOW CAN PARALLELIZE).
-        for (int ox = 0; ox < W; ox+=8) {
-            __m256 acc = _mm256_setzero_ps();
+        // THE NEEDED IMPLEMENTATION FOR SIMD FOR 128 BITS (16 BYTES => 4 FLOAT VALUES WHICH WE KNOW CAN PARALLELIZE).
+        for (int ox = 0; ox < W; ox+=4) {
+            __m128 acc = _mm_setzero_ps();
             for (int ky = 0; ky < K; ++ky) {
                 for (int kx = 0; kx < K; ++kx) {
-                    __m256 in_simd = _mm256_load_ps(in + ((oy + ky) * in_stride + (ox + kx)));
-                    __m256 ker_simd = _mm256_set1_ps(ker[ky * K + kx]);
-                    acc = _mm256_fmadd_ps(in_simd, ker_simd, acc);
+                    __m128 in_simd = _mm_load_ps(in + ((oy + ky) * in_stride + (ox + kx)));
+                    __m128 ker_simd = _mm_set1_ps(ker[ky * K + kx]);
+                    acc = _mm_fmadd_ps(in_simd, ker_simd, acc);
                 }
             }
-            _mm256_store_ps(out + (oy * W + ox), acc);
+            _mm_store_ps(out + (oy * W + ox), acc);
         }
+
+        // // THE NEEDED IMPLEMENTATION FOR SIMD FOR 256 BITS (32 BYTES => 8 FLOAT VALUES WHICH WE KNOW CAN PARALLELIZE).
+        // for (int ox = 0; ox < W; ox+=8) {
+        //     __m256 acc = _mm256_setzero_ps();
+        //     for (int ky = 0; ky < K; ++ky) {
+        //         for (int kx = 0; kx < K; ++kx) {
+        //             __m256 in_simd = _mm256_load_ps(in + ((oy + ky) * in_stride + (ox + kx)));
+        //             __m256 ker_simd = _mm256_set1_ps(ker[ky * K + kx]);
+        //             acc = _mm256_fmadd_ps(in_simd, ker_simd, acc);
+        //         }
+        //     }
+        //     _mm256_store_ps(out + (oy * W + ox), acc);
+        // }
 
         // THE NEEDED IMPLEMENTATION FOR SIMD FOR 512 BITS (64 BYTES => 16 FLOAT VALUES WHICH MIGHT FAIL FOR W NOT DIVISIBLE BY 16).
         // for (int ox = 0; ox < W; ox+=16) {
